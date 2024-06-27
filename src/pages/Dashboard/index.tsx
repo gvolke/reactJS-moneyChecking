448ec7599e7c614f985b20b6a262990e01000f49
Gustavo Volke <gustavo_volke@hotmail.com>
2024-06-26 22:06:54 -0300

    history page of all transactions

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,124 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useRef, useCallback, useEffect } from "react"
 
-import "react-day-picker/lib/style.css"
+import { FormHandles } from "@unform/core"
+import { Form } from "@unform/web"
 
-import { Container, Content, Header } from "./styles"
+import { format, parseISO } from "date-fns"
+
+import api from "../../services/api"
+import { useToast } from "../../hooks/toast"
+
+import {
+  Container,
+  Content,
+  Header,
+  Transactions,
+  TransactionCard,
+} from "./styles"
+
 import Sidebar from "../../components/Sidebar"
+import Input from "../../components/Input"
 
-import { FiPower } from "react-icons/fi"
-import { useAuth } from "../../hooks/auth"
+import { FiSearch } from "react-icons/fi"
+
+interface Transaction {
+  id: string
+  type: string
+  description: string
+  observation: string
+  user_id?: string
+  date: Date
+  value: number
+}
+
+interface TransactionBalance {
+  transaction: Transaction
+  balance: number
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut, user } = useAuth()
+  const formRef = useRef<FormHandles>(null)
+  const { addToast } = useToast()
+  const [transactions, setTransactions] = useState<TransactionBalance[]>([])
+
+  const handleSubmit = useCallback(
+    async (formData: any) => {
+      try {
+        let descriptionPar = ""
+        if (formData) {
+          descriptionPar = formData.search
+        }
+
+        const response = await api.get("/usertransactions/all", {
+          params: {
+            description: descriptionPar,
+          },
+        })
+
+        setTransactions(response.data)
+      } catch {
+        addToast({
+          type: "error",
+          title: "Erro na pesquisa",
+          description:
+            "Ocorreu um erro ao pesquisar os lançamentos, tente novamente",
+        })
+      }
+    },
+    [addToast]
+  )
+
+  useEffect(() => {
+    handleSubmit("")
+  }, [handleSubmit])
 
   return (
     <Container>
       <Sidebar />
 
       <Content>
-        <Header>Dashboard</Header>
+        <Header>Histórico Completo</Header>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input
+            name="search"
+            icon={FiSearch}
+            type="text"
+            placeholder="Pesquisar lançamentos por descrição"
+          />
+        </Form>
+
+        <Transactions>
+          {transactions.map((data) => (
+            <TransactionCard
+              key={data.transaction.id}
+              isOutcome={data.transaction.type === "SAIDA" ? true : false}
+            >
+              <span>{data.transaction.type}</span>
+
+              <span>
+                {`${format(
+                  parseISO(data.transaction.date.toString()),
+                  "dd/MM/yyyy"
+                ).toString()}`}
+              </span>
+
+              <span>{data.transaction.description}</span>
+
+              <span>{data.transaction.observation}</span>
+
+              <span>
+                {`Valor: R$ ${data.transaction.value.toLocaleString("pt-BR", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })} | Saldo: R$ ${data.balance.toLocaleString("pt-BR", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}`}
+              </span>
+            </TransactionCard>
+          ))}
+        </Transactions>
       </Content>
     </Container>
   )
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,50 +1,85 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { shade } from "polished"
 
-export const Container = styled.div`
-  height: 100vh;
+interface transactionCardProps {
+  isOutcome: boolean
+}
 
+export const Container = styled.div`
   display: flex;
   flex-direction: row;
+
+  height: 100vh;
 `
 
 export const Header = styled.header`
   background: #f5f5f5;
   font-size: 32px;
-`
+  margin-bottom: 20px;
 
-export const Profile = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 80px;
-
-  img {
-    width: 56px;
-    height: 56px;
-    border-radius: 50%;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
-    margin-left: 16px;
-    line-height: 24px;
-
-    span {
-      color: #5b5b5b;
-    }
-
-    a {
-      text-decoration: none;
-      color: #4169e1;
-    }
-
-    a:hover {
-      opacity: 0.8;
-    }
-  }
+  height: 30px;
 `
 
 export const Content = styled.main`
-  margin: 15px;
+  padding: 20px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  Form {
+    height: 60px;
+    margin-bottom: 10px;
+  }
+`
+
+export const Transactions = styled.div`
+  width: 100%;
+  flex: 1;
+
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: white;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${shade(0.1, "#00a6ff")};
+    border-radius: 5px;
+  }
+
+  /* Handle ao passar o mouse */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #b4ccfc;
+  }
+`
+
+export const TransactionCard = styled.div<transactionCardProps>`
+  border: #4169e1 solid 2px;
+  border-radius: 8px;
+  margin-right: 5px;
+
+  color: black;
+  font-size: 18px;
+  padding: 10px;
+  margin-bottom: 10px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  span:first-of-type {
+    color: green;
+
+    ${(props) =>
+      props.isOutcome &&
+      css`
+        color: #dc143c;
+      `}
+    font-weight: 500;
+  }
 `
